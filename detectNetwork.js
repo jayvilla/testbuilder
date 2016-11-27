59198860e949703b0bfcc915ae324dff64546a71
Jeff Villarina <villarina.jeffrey@gmail.com>
2016-11-26 17:43:11 -0800

    CREATED working detectNetwork funct

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,18 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+  var firstTwo = cardNumber.substr(0,2);
+  var length = cardNumber.split('').length;
+  
+  if (firstTwo === '38' || firstTwo === '39' && length === 14) {
+  	return 'Diner\'s Club';
+  } else if (firstTwo === '34' || firstTwo === '37' && length == 15) {
+  	return 'American Express'
+  }
 };
 
+console.log(detectNetwork('38345678901234'));
+console.log(detectNetwork('39345678901234'));
+console.log(detectNetwork('343456789012345'));
+console.log(detectNetwork('373456789012345'));
 
