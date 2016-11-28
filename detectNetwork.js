e7c3603bd9856877880baa3d6474678bccbc25c9
Jeff Villarina <villarina.jeffrey@gmail.com>
2016-11-27 17:14:48 -0800

    finalized version all test inputs work

--- detectNetwork.js
+++ detectNetwork.js
@@ -12,19 +12,19 @@ var detectNetwork = function(cardNumber) {
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
-  var prefix = cardNumber.substr(0,2);
-  var length = cardNumber.split('').length;
-  
-  if (prefix === '38' || prefix === '39' && length === 14) {
-  	return 'Diner\'s Club';
-  } else if (prefix === '34' || prefix === '37' && length == 15) {
-  	return 'American Express'
+  if (cardNumber.length == 14 && (cardNumber.startsWith('38') || cardNumber.startsWith('39'))) {
+  	return "Diner\'s Club";
   }
+  
+  if (cardNumber.length == 15 && (cardNumber.startsWith('34') || cardNumber.startsWith('37'))) {
+  	return "American Express";
+  }
+  // Once you've read this, go ahead and try to implement this function, then return to the console.
+
 };
 
-console.log(detectNetwork('38345678901234'));
-console.log(detectNetwork('39345678901234'));
-console.log(detectNetwork('343456789012345'));
-console.log(detectNetwork('373456789012345'));
+detectNetwork('38345678901234');
+detectNetwork('39345678901234');
+detectNetwork('343456789012345');
+detectNetwork('373456789012345');
 
